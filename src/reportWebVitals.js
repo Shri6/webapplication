d6e69040da62f96606e826d290599a84c0639887
Shrinidhi A <shrinidhisringeri@gmail.com>
2022-05-27 18:56:34 +0530

    1

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,13 @@
-const reportWebVitals = (onperfEntry) => {
-    if (onperfEntry && onperfEntry instanceof Function) {
-      import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-        getCLS(onperfEntry);
-        getFID(onperfEntry);
-        getFCP(onperfEntry);
-        getLCP(onperfEntry);
-        getTTFB(onperfEntry);
-      });
-    }
-  };
-  export default reportWebVitals;
-  
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
